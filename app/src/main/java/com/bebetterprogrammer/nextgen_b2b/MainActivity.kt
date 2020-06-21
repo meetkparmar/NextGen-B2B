1fc6e6d9df96edf6055b75c2568d09ed079a7581
Meet Parmar <meetkumar.k.parmar@gmail.com>
2020-06-21 13:11:41 +0530

    Create an Empty project

---
+++ app/src/main/java/com/bebetterprogrammer/nextgen_b2b/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bebetterprogrammer.nextgen_b2b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
